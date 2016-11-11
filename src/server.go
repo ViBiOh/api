66bd6bc4415c3cc67c60aa98c2be2c56b6127971
Vincent Boutour <bob@vibioh.fr>
2016-11-11 13:58:29 +0100

    Removing morningStar

--- src/server.go
+++ src/server.go
@@ -3,13 +3,11 @@ package main
 import "net/http"
 import "log"
 import "./hello"
-import "./morningStar"
 
 const port = `1080`
 
 func main() {
 	http.HandleFunc(`/hello/`, hello.Handler)
-	http.HandleFunc(`/morningStar/`, morningStar.Handler)
 
 	log.Print(`Starting server on port ` + port)
 	log.Fatal(http.ListenAndServe(`:`+port, nil))
