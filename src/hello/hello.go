589ea644d5a8451fd575204e98e287abf875e800
Vincent Boutour <bob@vibioh.fr>
2016-11-17 23:45:00 +0100

    Adding OWASP header

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -19,6 +19,11 @@ func pluralize(s string, n int) string {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add(`Access-Control-Allow-Origin`, `*`)
+	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type`)
+	w.Header().Add(`Access-Control-Allow-Methods`, `GET`)
+	w.Header().Add(`X-Content-Type-Options`, `nosniff`)
+
 	hello := Hello{`Hello ` + html.EscapeString(strings.Replace(r.URL.Path, `/hello/`, ``, -1)) + `, I'm greeting you from the server!`}
 
 	jsonHttp.ResponseJson(w, hello)
