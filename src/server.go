570e5fdbf141169093f72f6cc02ceb8d01a17452
Vincent <ViBiOh@users.noreply.github.com>
2016-11-18 14:00:38 +0100

    Update server.go

--- src/server.go
+++ src/server.go
@@ -5,9 +5,10 @@ import "log"
 import "./hello"
 
 const port = `1080`
+const HELLO_PATH = `/hello/`
 
 func main() {
-	http.HandleFunc(`/hello/`, hello.Handler)
+	http.HandleFunc(HELLO_PATH, http.StripPrefix(HELLO_PATH, hello.Handler)
 
 	log.Print(`Starting server on port ` + port)
 	log.Fatal(http.ListenAndServe(`:`+port, nil))
