fe09fa754e2b0e0e48ae629c56ee93d1ce70976e
Vincent <ViBiOh@users.noreply.github.com>
2017-03-28 09:31:40 +0200

    Fmt

--- hello/hello.go
+++ hello/hello.go
@@ -28,7 +28,7 @@ func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type`)
 	w.Header().Add(`Access-Control-Allow-Methods`, `GET`)
 	w.Header().Add(`X-Content-Type-Options`, `nosniff`)
-	
+
 	if r.Method == http.MethodOptions {
 		w.Write(nil)
 		return
