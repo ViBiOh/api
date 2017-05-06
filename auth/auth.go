d8e587e34ab461786a5d586f70c8416f80b12926
Vincent Boutour <bob@vibioh.fr>
2017-05-06 16:43:52 +0200

    Adding auth handler

---
+++ auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"net/http"
+)
+
+// Handler for Hello request. Should be use with net/http
+type Handler struct {
+}
+
+func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add(`Access-Control-Allow-Origin`, `*`)
+	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type`)
+	w.Header().Add(`Access-Control-Allow-Methods`, `GET`)
+	w.Header().Add(`X-Content-Type-Options`, `nosniff`)
+
+	if r.Method == http.MethodOptions {
+		w.Write(nil)
+		return
+	}
+
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write(nil)
+}
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/ViBiOh/go-api/auth"
 	"github.com/ViBiOh/go-api/echo"
 	"github.com/ViBiOh/go-api/hello"
 	"log"
@@ -11,12 +12,14 @@ import (
 const port = `1080`
 const helloPath = `/hello/`
 const echoPath = `/echo/`
+const authPath = `/auth/`
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	http.Handle(helloPath, http.StripPrefix(helloPath, hello.Handler{}))
 	http.Handle(echoPath, http.StripPrefix(echoPath, echo.Handler{}))
+	http.Handle(authPath, http.StripPrefix(authPath, auth.Handler{}))
 
 	log.Print(`Starting server on port ` + port)
 	log.Fatal(http.ListenAndServe(`:`+port, nil))
