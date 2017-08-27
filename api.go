e1c7eb9ef17cb49764f7dc2730426135f98e6201
Vincent Boutour <bob@vibioh.fr>
2017-08-27 12:32:02 +0200

    Removing regexp for performance sake

--- api.go
+++ api.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"regexp"
 	"runtime"
+	"strings"
 
 	"github.com/ViBiOh/alcotest/alcotest"
 	"github.com/ViBiOh/go-api/auth"
@@ -22,33 +22,23 @@ import (
 const port = `1080`
 
 const helloPath = `/hello`
-
-var helloRequestMatcher = regexp.MustCompile(`^` + helloPath)
-var helloHandler = http.StripPrefix(helloPath, hello.Handler{})
-
 const echoPath = `/echo`
-
-var echoRequestMatcher = regexp.MustCompile(`^` + echoPath)
-var echoHandler = http.StripPrefix(echoPath, echo.Handler{})
-
 const authPath = `/auth`
-
-var authRequestMatcher = regexp.MustCompile(`^` + authPath)
-var authHandler = http.StripPrefix(authPath, auth.Handler{})
-
 const healthcheckPath = `/health`
 
-var healthcheckRequestMatcher = regexp.MustCompile(`^` + healthcheckPath)
+var helloHandler = http.StripPrefix(helloPath, hello.Handler{})
+var echoHandler = http.StripPrefix(echoPath, echo.Handler{})
+var authHandler = http.StripPrefix(authPath, auth.Handler{})
 var healthcheckHandler = http.StripPrefix(healthcheckPath, healthcheck.Handler{})
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-	if helloRequestMatcher.MatchString(r.URL.Path) {
+	if strings.HasPrefix(r.URL.Path, helloPath) {
 		helloHandler.ServeHTTP(w, r)
-	} else if echoRequestMatcher.MatchString(r.URL.Path) {
+	} else if strings.HasPrefix(r.URL.Path, echoPath) {
 		echoHandler.ServeHTTP(w, r)
-	} else if authRequestMatcher.MatchString(r.URL.Path) {
+	} else if strings.HasPrefix(r.URL.Path, authPath) {
 		authHandler.ServeHTTP(w, r)
-	} else if healthcheckRequestMatcher.MatchString(r.URL.Path) {
+	} else if strings.HasPrefix(r.URL.Path, healthcheckPath) {
 		healthcheckHandler.ServeHTTP(w, r)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -57,6 +47,7 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	url := flag.String(`c`, ``, `URL to check`)
+	tls := flag.Bool(`tls`, false, `Serve TLS content`)
 	flag.Parse()
 
 	if *url != `` {
@@ -73,6 +64,10 @@ func main() {
 		Handler: prometheus.NewPrometheusHandler(`http`, owasp.Handler{Handler: cors.Handler{Handler: http.HandlerFunc(apiHandler)}}),
 	}
 
-	go log.Panic(cert.ListenAndServeTLS(server))
+	if *tls {
+		go log.Print(cert.ListenAndServeTLS(server))
+	} else {
+		go log.Print(server.ListenAndServe())
+	}
 	httputils.ServerGracefulClose(server, nil)
 }
