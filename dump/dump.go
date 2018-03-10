fd0ec336f0215e8c971070ed47e28aa75fecbbb3
Vincent Boutour <bob@vibioh.fr>
2018-03-10 13:43:21 +0100

    Fixing string concatenation

--- dump/dump.go
+++ dump/dump.go
@@ -1,6 +1,7 @@
 package dump
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,22 +13,22 @@ import (
 // Handler for dump request. Should be use with net/http
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var headers string
+		var headers bytes.Buffer
 		for key, value := range r.Header {
-			headers += fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`))
+			headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`)))
 		}
 
-		var params string
+		var params bytes.Buffer
 		for key, value := range r.URL.Query() {
-			params += fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`))
+			headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`)))
 		}
 
-		var form string
+		var form bytes.Buffer
 		if err := r.ParseForm(); err != nil {
-			form = fmt.Sprintf(`Error while parsing form: %v`, err)
+			form.WriteString(fmt.Sprintf(`Error while parsing form: %v`, err))
 		} else {
 			for key, value := range r.PostForm {
-				form += fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`))
+				form.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, `,`)))
 			}
 		}
 
@@ -36,32 +37,33 @@ func Handler() http.Handler {
 			log.Printf(`Error while reading body: %v`, err)
 		}
 
-		outputPattern := "\n%s %s\n"
+		var outputPattern bytes.Buffer
+		outputPattern.WriteString("\n%s %s\n")
 		outputData := []interface{}{
 			r.Method,
 			r.URL.Path,
 		}
 
-		if headers != `` {
-			outputPattern += "Headers\n%s\n"
-			outputData = append(outputData, headers)
+		if headers.Len() != 0 {
+			outputPattern.WriteString("Headers\n%s\n")
+			outputData = append(outputData, headers.String())
 		}
 
-		if params != `` {
-			outputPattern += "Params\n%s\n"
-			outputData = append(outputData, params)
+		if params.Len() != 0 {
+			outputPattern.WriteString("Params\n%s\n")
+			outputData = append(outputData, params.String())
 		}
 
-		if form != `` {
-			outputPattern += "Form\n%s\n"
-			outputData = append(outputData, form)
+		if form.Len() != 0 {
+			outputPattern.WriteString("Form\n%s\n")
+			outputData = append(outputData, form.String())
 		}
 
 		if len(body) != 0 {
-			outputPattern += "Body\n%s\n"
+			outputPattern.WriteString("Body\n%s\n")
 			outputData = append(outputData, body)
 		}
 
-		log.Printf(outputPattern, outputData...)
+		log.Printf(outputPattern.String(), outputData...)
 	})
 }
