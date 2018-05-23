eba8e1b6370f837e071fb12c55ccdf191ccfb755
Vincent Boutour <bob@vibioh.fr>
2018-05-23 22:46:25 +0200

    Fixing error check when writing

--- pkg/dump/dump.go
+++ pkg/dump/dump.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/ViBiOh/httputils/pkg/dump"
+	"github.com/ViBiOh/httputils/pkg/httperror"
 )
 
 // Handler for dump request. Should be use with net/http
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(dump.Request(r)))
+		if _, err := w.Write([]byte(dump.Request(r))); err != nil {
+			httperror.InternalServerError(w, err)
+		}
 	})
 }
