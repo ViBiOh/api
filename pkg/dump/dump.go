0c23ed561550deca1483951194e450ba9b0df483
Vincent Boutour <bob@vibioh.fr>
2019-07-27 15:57:59 +0200

    Bumping httputils

--- pkg/dump/dump.go
+++ pkg/dump/dump.go
@@ -1,18 +1,21 @@
 package dump
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
+	"strings"
 
-	"github.com/ViBiOh/httputils/pkg/dump"
 	"github.com/ViBiOh/httputils/pkg/errors"
 	"github.com/ViBiOh/httputils/pkg/httperror"
 	"github.com/ViBiOh/httputils/pkg/logger"
+	"github.com/ViBiOh/httputils/pkg/request"
 )
 
 // Handler for dump request. Should be use with net/http
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		value := dump.Request(r)
+		value := dumpRequest(r)
 
 		logger.Info("Dump of request\n%s", value)
 
@@ -20,4 +23,59 @@ func Handler() http.Handler {
 			httperror.InternalServerError(w, errors.WithStack(err))
 		}
 	})
+}
+
+func dumpRequest(r *http.Request) string {
+	var headers bytes.Buffer
+	for key, value := range r.Header {
+		headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
+	}
+
+	var params bytes.Buffer
+	for key, value := range r.URL.Query() {
+		headers.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
+	}
+
+	var form bytes.Buffer
+	if err := r.ParseForm(); err != nil {
+		form.WriteString(err.Error())
+	} else {
+		for key, value := range r.PostForm {
+			form.WriteString(fmt.Sprintf("%s: %s\n", key, strings.Join(value, ",")))
+		}
+	}
+
+	body, err := request.ReadBodyRequest(r)
+	if err != nil {
+		logger.Error("%#v", err)
+	}
+
+	var outputPattern bytes.Buffer
+	outputPattern.WriteString("%s %s\n")
+	outputData := []interface{}{
+		r.Method,
+		r.URL.Path,
+	}
+
+	if headers.Len() != 0 {
+		outputPattern.WriteString("Headers\n%s\n")
+		outputData = append(outputData, headers.String())
+	}
+
+	if params.Len() != 0 {
+		outputPattern.WriteString("Params\n%s\n")
+		outputData = append(outputData, params.String())
+	}
+
+	if form.Len() != 0 {
+		outputPattern.WriteString("Form\n%s\n")
+		outputData = append(outputData, form.String())
+	}
+
+	if len(body) != 0 {
+		outputPattern.WriteString("Body\n%s\n")
+		outputData = append(outputData, body)
+	}
+
+	return fmt.Sprintf(outputPattern.String(), outputData...)
 }
